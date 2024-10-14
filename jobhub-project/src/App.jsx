67eba22b24f0a11e22848d11462afeac2d644022
Ankita <deeparajput5348@gmail.com>
2024-10-14 11:15:19 +0530

    first commit

---
+++ jobhub-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="text-gray-700 text-4xl">Hello World!</div>
+    </>
+  )
+}
+
+export default App
