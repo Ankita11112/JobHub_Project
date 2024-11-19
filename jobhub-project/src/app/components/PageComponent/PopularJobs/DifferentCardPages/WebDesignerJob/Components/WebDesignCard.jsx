d3fb892ace2107a0e1a4da72d5c86eb28d04a894
Ankita <deeparajput5348@gmail.com>
2024-11-19 12:45:49 +0530

    webdesigner page done

--- jobhub-project/src/app/components/PageComponent/PopularJobs/DifferentCardPages/WebDesignerJob/Components/WebDesignCard.jsx
+++ jobhub-project/src/app/components/PageComponent/PopularJobs/DifferentCardPages/WebDesignerJob/Components/WebDesignCard.jsx
@@ -8,29 +8,6 @@ import webImg2 from '../../../../../../assets/Images/WebDesignImgs/webImg2.png';
 import webImg3 from '../../../../../../assets/Images/WebDesignImgs/webImg3.png';
 
 const WebDesignCard = () => {
-  const cardContent = [
-    {
-      img: WebDesignImg,
-      title: "Front-End Designer",
-      description:
-        "A Front-End Designer combines design and coding skills to create visually appealing, responsive websites using HTML, CSS, and JavaScript.",
-      img1: webImg1,
-    },
-    {
-      img: BrandContentImg,
-      title: "Brand and Content Designer",
-      description:
-        "Creates cohesive visual identities, engaging content, and compelling brand stories that resonate and connect with people.",
-      img1: webImg2,
-    },
-    {
-      img: UiUxDesignImg,
-      title: "UI/UX Designer",
-      description:
-        "Creates user-friendly, people-centered designs that simplify interactions and elevate the digital experience.",
-      img1: webImg3,
-    },
-  ];
 
   const containerStyles = {
     display: 'flex',
@@ -39,24 +16,6 @@ const WebDesignCard = () => {
     flexGrow: 1,
     mb: 10,
     px: 5,
-  };
-
-  const cardStyles = {
-    position: 'relative',
-    '&:hover .hiddenImg': {
-      transform: 'translateX(-100px)', // Smoothly move the image to the left
-    },
-  };
-
-  const hiddenImg = {
-    position: 'absolute',
-    top: '66%',
-    left: '2%',
-    transform: 'translateX(-100%, -100%)', // Center the image
-    width: '150px',
-    height: '150px',
-    transition: 'transform 0.5s ease', // Smooth transition
-    zIndex: -1, // Keep the image behind the card
   };
 
   return (
@@ -90,60 +49,210 @@ const WebDesignCard = () => {
       </Grid2>
 
       <Grid2 container spacing={5} sx={{ width: '80%' }}>
-        {cardContent.map((item, index) => (
-          <Grid2
-            item
-            key={index}
-            xs={12}
-            sm={8}
-            md={4}
-            sx={{ width: { sm: '100%', md: '30%' }, ...cardStyles }}
+        <Grid2
+          item
+          xs={12}
+          sm={8}
+          md={4}
+          sx={{
+            width: { sm: '100%', md: '30%' }, borderRadius: "5px",
+            border:
+              "8px solid green",
+            position: 'relative',
+            '&:hover .hiddenImg': {
+              transform: {md: 'translateX(-100px)'},
+            },
+          }}
+        >
+          <Card sx={{ width: '100%', height: '450px' }}>
+            <img
+              src={WebDesignImg}
+              style={{ width: '100%', height: '50%' }}
+              alt='illustration'
+            />
+            <CardContent sx={{ height: '100%' }}>
+              <Typography
+                variant="h4"
+                component="h3"
+                sx={{
+                  fontSize: { xs: '22px', md: '30px' },
+                  textAlign: 'center',
+                }}
+              >
+                Front-End Designer
+              </Typography>
+              <Typography
+                variant="body2"
+                component="h6"
+                sx={{
+                  textAlign: 'justify',
+                  fontSize: { xs: '13px', md: '17px' },
+                  pt: 2,
+                  pb: 3
+                }}
+              >
+                A Front-End Designer combines design and coding skills to create visually appealing, responsive websites using HTML, CSS, and JavaScript.
+              </Typography>
+            </CardContent>
+          </Card>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '66%',
+              left: '2%',
+              transform: 'translateX(-100%, -100%)',
+              width: '150px',
+              height: '150px',
+              transition: 'transform 0.5s ease',
+              zIndex: -1,
+            }}
+            className="hiddenImg"
           >
-            <Card sx={{ width: '100%', height: '450px' }}>
-              <img
-                src={item.img}
-                style={{ width: '100%', height: '50%' }}
-                alt={`${item.title} illustration`}
-              />
-              <CardContent sx={{ height: '100%' }}>
-                <Typography
-                  variant="h4"
-                  component="h3"
-                  sx={{
-                    fontSize: { xs: '22px', md: '30px' },
-                    textAlign: 'center',
-                  }}
-                >
-                  {item.title}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  component="h6"
-                  sx={{
-                    textAlign: 'justify',
-                    fontSize: { xs: '13px', md: '17px' },
-                    pt: index === 0 ? 2 : index === 1 ? 4 : 6,
-                    pb: index === 0 ? 3 : index === 1 ? 6 : 7,
-                  }}
-                >
-                  {item.description}
-                </Typography>
-              </CardContent>
-            </Card>
-            <Box
-              sx={{
-                ...hiddenImg,
-              }}
-              className="hiddenImg"
-            >
-              <img
-                src={item.img1}
-                style={{ height: '100%', width: '100%' }}
-                alt={`${item.title} cartoon`}
-              />
-            </Box>
-          </Grid2>
-        ))}
+            <img
+              src={webImg1}
+              style={{ height: '100%', width: '100%' }}
+              alt='cartoon'
+            />
+          </Box>
+        </Grid2>
+
+        <Grid2
+          item
+          xs={12}
+          sm={8}
+          md={4}
+          sx={{
+            width: { sm: '100%', md: '30%' }, 
+            top: {md: "2rem"},
+            borderRadius: "5px",
+            border:
+              "8px solid green",
+            position: 'relative',
+            '&:hover .hiddenImg': {
+              transform: {md: 'translate(-45px, -40px)'},
+            },
+          }}
+        >
+          <Card sx={{ width: '100%', height: '450px' }}>
+            <img
+              src={BrandContentImg}
+              style={{ width: '100%', height: '50%' }}
+              alt='illustration'
+            />
+            <CardContent sx={{ height: '100%' }}>
+              <Typography
+                variant="h4"
+                component="h3"
+                sx={{
+                  fontSize: { xs: '22px', md: '30px' },
+                  textAlign: 'center',
+                }}
+              >
+                Brand and Content Designer
+              </Typography>
+              <Typography
+                variant="body2"
+                component="h6"
+                sx={{
+                  textAlign: 'justify',
+                  fontSize: { xs: '13px', md: '17px' },
+                  pt: 2,
+                  pb: 3
+                }}
+              >
+                Creates cohesive visual identities, engaging content, and compelling brand stories that resonate and connect with people.
+              </Typography>
+            </CardContent>
+          </Card>
+          <Box
+           sx={{
+              position: 'absolute',
+              top: '90%',
+              left: "-10px",
+              transform: 'translate(50%, -80%)',
+              width: '150px',
+              height: '150px',
+              transition: 'transform 0.5s ease',
+              zIndex: -1,
+            }}
+            className="hiddenImg"
+          >
+            <img
+              src={webImg2}
+              style={{ height: '100%', width: '100%' }}
+              alt='cartoon'
+            />
+          </Box>
+        </Grid2>
+
+        <Grid2
+          item
+          xs={12}
+          sm={8}
+          md={4}
+          sx={{
+            width: { sm: '100%', md: '30%' }, 
+            top: {md: "4rem"},
+            borderRadius: "5px",
+            border:
+              "8px solid green",
+            position: 'relative',
+            '&:hover .hiddenImg': {
+              transform: {md: 'translateY(-155px)'},
+            },
+          }}
+        >
+          <Card sx={{ width: '100%', height: '450px' }}>
+            <img
+              src={UiUxDesignImg}
+              style={{ width: '100%', height: '50%' }}
+              alt='illustration'
+            />
+            <CardContent sx={{ height: '100%' }}>
+              <Typography
+                variant="h4"
+                component="h3"
+                sx={{
+                  fontSize: { xs: '22px', md: '30px' },
+                  textAlign: 'center',
+                }}
+              >
+                UI/UX Designer
+              </Typography>
+              <Typography
+                variant="body2"
+                component="h6"
+                sx={{
+                  textAlign: 'justify',
+                  fontSize: { xs: '13px', md: '17px' },
+                  pt: 2,
+                  pb: 3
+                }}
+              >
+                Creates user-friendly, people-centered designs that simplify interactions and elevate the digital experience.
+              </Typography>
+            </CardContent>
+          </Card>
+          <Box
+           sx={{
+              position: 'absolute',
+              top: '0%',
+              left: {md: '55%'},
+              transform: 'translateY(50%)',
+              width: '150px',
+              height: '150px',
+              transition: 'transform 0.5s ease',
+              zIndex: -1,
+            }}
+            className="hiddenImg"
+          >
+            <img
+              src={webImg3}
+              style={{ height: '100%', width: '100%' }}
+              alt='cartoon'
+            />
+          </Box>
+        </Grid2>
       </Grid2>
     </Box>
   );
