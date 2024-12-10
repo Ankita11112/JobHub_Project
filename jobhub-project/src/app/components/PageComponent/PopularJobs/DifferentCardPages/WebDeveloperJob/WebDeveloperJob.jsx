27fb628436ca5a9c19eaebaf23b51905e8cb88d4
Shubham Kumar <sk6930037@gmail.com>
2024-12-10 14:37:57 +0530

    changes

--- jobhub-project/src/app/components/PageComponent/PopularJobs/DifferentCardPages/WebDeveloperJob/WebDeveloperJob.jsx
+++ jobhub-project/src/app/components/PageComponent/PopularJobs/DifferentCardPages/WebDeveloperJob/WebDeveloperJob.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import PageComponent from '../../../PageComponent'
+import React from 'react';
+import PageComponent from '../../../PageComponent';
 import bgImage from '../../../../../assets/Images/bgImages/bgImage.png';
 import HeaderImage from '../../../../../assets/Images/WebDeveloper/header.png';
 import HeaderPage from '../../../../GlobalComponents/HeaderPage/HeaderPage'
@@ -21,26 +21,22 @@ const WebDeveloperJob = () => {
           {<>
             Web<span style={{ color: "green" }}>Developer</span>
           </>}
-          subText="Skilled in creating responsive, user-friendly websites using front-end and back-end technologies."
+          subText="Friendly telecaller providing customer support, handling inquiries, and boosting client satisfaction."
           buttonTitle1="Apply Now"
           buttonTitle2="Search"
           // onButtonClick1={handleScrollView}
           onButtonClick1={() => navigate('/registration')}
-          onButtonClick2={() => alert("Hire Now clicked!")}
+          onButtonClick2={() => alert('Hire Now clicked!')}
           imgSrc={HeaderImage}
-          style={{
-            width: "75%",
-            height: "auto",
+          style= {{
+            width: "75%", 
+            height: "auto", 
           }}
         />
-        <WebDevCat />
-        <JobDetails />
-        <WebDevFirstDes />
-        <WebDevCrousal />
-        <WebDevSecDes/>
+      <JobDetails/>
       </PageComponent>
     </>
-  )
-}
+  );
+};
 
 export default WebDeveloperJob;
--- jobhub-project/src/app/source/Pages/Admin/SigninUpPage/SignIn/LogIn.jsx
+++ jobhub-project/src/app/source/Pages/Admin/SigninUpPage/SignIn/LogIn.jsx
@@ -13,7 +13,7 @@ const LogIn = () => {
     borderRadius: '10px',
     boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
     backgroundColor: 'rgba(255, 255, 255, 0.9)',
-    margin: '10px',
+    
   };
   const signup = { marginTop: '10px' };
   const btnstyle = {
