d37dc42826158b6214e4546f515b5e30a03ac86c
Ayush Kumar <ayush.dhiman0610@gmail.com>
2024-12-14 11:10:55 +0530

    last-commit

--- jobhub-project/src/app/components/PageComponent/PopularJobs/DifferentCardPages/SecurityJob/SecurityJob.jsx
+++ jobhub-project/src/app/components/PageComponent/PopularJobs/DifferentCardPages/SecurityJob/SecurityJob.jsx
@@ -7,8 +7,8 @@ import JobDetails from '../../../JobDetails/JobDetails';
 import { useNavigate } from 'react-router-dom';
 import SecCat from './components/SecCat';
 
-import FirImg from '../../../../../assets/Images/HotelImages/first.png';
-import SecImg from '../../../../../assets/Images/HotelImages/second.png';
+import FirImg from '../../../../../assets/Images/GuardImages/first.png';
+import SecImg from '../../../../../assets/Images/GuardImages/Second.png';
 import tickImg from '../../../../../assets/Images/DocVerifyImage/tickImg.png';
 import GlobalJobCardOne from '../../../../GlobalComponents/GlobalGrid2Section/GlobalJobCardOne';
 import GlobalJobCardTwo from '../../../../GlobalComponents/GlobalGrid2Section/GlobalJobCardTwo';
@@ -49,38 +49,38 @@ const SecurityJob = () => {
         <JobDetails />
 
 
-              {/* First Desc */}
-              <GlobalJobCardOne
-          title={"Hotel Staff "}
-          subtitle={"Hotel employees are responsible for guest satisfaction by offering services like registration, accommodation cleaning services, food service and offering response to guests to improve satisfaction."}
+        {/* First Desc */}
+        <GlobalJobCardOne
+          title={"Security Guard"}
+          subtitle={"The duty of a security guard is to safely and securely guard premises and property, and protect people. Their purpose is to keep them secured to secure an unauthorizedaccess, monitors their activities as well as minimizes the emergencies.  "}
           content={
 
             [
               {
                 icon: tickImg,
                 description:
-                  '<strong> Guest Assistance:</strong> Also meet the guests needs in cases like answering their questions, or helping them with the check-ins/outs and in fact fending for the guests comfort.',
+                  '<strong> Surveillance and Monitoring:</strong> They protect life on patrol areas, monitor CCTVs and alarms; and detect security breach.',
               },
               {
                 icon: tickImg,
                 description:
-                  '<strong>Housekeeping:</strong>Clean the rooms and official places which are part of guest rooms and other areas accessible to the guests.',
+                  '<strong>Access Control:</strong> Revising to, Identify occupying own to b justify, maintain visitors and vehicle records and restrain unauthorized accession and protection.',
               },
               {
                 icon: tickImg,
                 description:
-                  '<strong>Dining Services:</strong>Please guests by bringing the orders, taking reservations and offering to guests in so far as meals are concerned.',
+                  '<strong>Emergency Response:</strong> Alerts to alarms, and assists evacuations or medical situations, when required for service to emergency.',
               },
 
               {
                 icon: tickImg,
                 description:
-                  '<strong>Problem Resolution:</strong>Attend to customers and solve their complaints and other related concerns in a manner which meets hotel quality.',
+                  '<strong>Property Protection:</strong> It will help limit theft and damage as well as check up on locks and windows and secure your assets.',
               },
               {
                 icon: tickImg,
                 description:
-                  '<strong>Safety and Security:</strong> Writing, practicing, put into action safety protocols and basically creating a favorable environment for safe keeping both staffs and guests.',
+                  '<strong>Documentation and Enforcement:</strong> Work includes logging daily activities, preparing incident reports, being in charge., dealing with disputes calmly and ensuring strict compliance of security protocols.',
               },
 
 
@@ -96,41 +96,32 @@ const SecurityJob = () => {
               {
                 icon: tickImg,
                 description:
-                  '<strong>Clerical Desk Services:</strong>  Employees handle guest registration, check-in, check-out, and address all guest inquiries.',
+                  '<strong>Protection Measures:</strong>  Minimize or avoid the risks caused by incidents to affect people, assets, and environments.',
               },
 
               {
                 icon: tickImg,
                 description:
-                  '<strong> Housekeeping:</strong> Staff maintain clean rooms to ensure guests enjoy a hygienic and comfortable environment.',
+                  '<strong> Core Activities:</strong>  Complement the physical security risks through surveillance, access control and crises risks.',
               },
 
               {
                 icon: tickImg,
                 description:
-                  '<strong>Catering and Dining:</strong> Cottages prepare and serve first-class meals, adhering to dining standards and providing excellent service.',
+                  '<strong> Prevention & Deterrence: </strong>Maintain secure and prevent the criminal act to guarantee security.',
               },
 
               {
                 icon: tickImg,
                 description:
-                  '<strong> Information and Baggage Services:</strong>  Staff assist with guest information and manage baggage services efficiently.',
+                  '<strong>Physical Security: </strong>  Slows or halts human aggressiveness on people, property, and events..',
               },
               {
                 icon: tickImg,
                 description:
-                  '<strong>Repair Services:</strong>   The maintenance team addresses any machinery issues to ensure smooth operations.',
+                  '<strong>Digital Security:</strong> A technology that protect networks, systems, and software against cyber threats..',
               },
-              {
-                icon: tickImg,
-                description:
-                  '<strong> Security:</strong> The security department handles all safety and security concerns to ensure guest safety.',
-              },
-              {
-                icon: tickImg,
-                description:
-                  '<strong> Organizational Goal:</strong> All departments work together to ensure guest comfort, hospitality, and overall satisfaction.',
-              },
+          
 
 
             ]
