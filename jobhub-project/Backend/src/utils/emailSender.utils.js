c8923392d923a6c565f5e9e6fc4932518320b6d4
Ankita <deeparajput5348@gmail.com>
2024-12-17 15:02:28 +0530

    update code added some flags

--- jobhub-project/Backend/src/utils/emailSender.utils.js
+++ jobhub-project/Backend/src/utils/emailSender.utils.js
@@ -4,10 +4,14 @@ export const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
       },
+      tls: {
+        rejectUnauthorized: false,
+      }
     });
     let info = await transporter.sendMail({
       from: `JobHub || Job-Provider`,
