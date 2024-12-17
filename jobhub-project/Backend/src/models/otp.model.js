10a0577f3b6f3fcefcedb1175817f84ad1a7889e
Ankita <deeparajput5348@gmail.com>
2024-12-17 14:42:03 +0530

    Added models for admin, employee,  job, otp and student

---
+++ jobhub-project/Backend/src/models/otp.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const otpSchema = new Schema({
+  mobileNumber: {
+    type: Number,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 5 * 60,
+  },
+});
+
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification for Email",
+      emailTemplate(otp)
+    );
+    console.log("Otp send successfully", mailResponse.response);
+  } catch (err) {
+    console.error("Error in send verification function in otp model", err);
+    throw err;
+  }
+}
+
+otpSchema.pre("save", async function (next) {
+  if (this.isnew) {
+    await sendVerificationEmail(this.email, this.otp);
+  }
+  next();
+});
+
+export const Otp = mongoose.model("Otp", otpSchema);
