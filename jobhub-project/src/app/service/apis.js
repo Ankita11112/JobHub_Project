ca002fa2a748c1893be861467d116331daed4826
Ankita <deeparajput5348@gmail.com>
2024-12-19 00:39:17 +0530

    added direct link for testing

--- jobhub-project/src/app/service/apis.js
+++ jobhub-project/src/app/service/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = import.meta.env.BASE_URL;
+// const BASE_URL = import.meta.env.BASE_URL;
+const BASE_URL = "http://localhost:3000/api/v1";
 
 export const employee = {
   SEND_OTP: BASE_URL + "/employees/otp-send",
