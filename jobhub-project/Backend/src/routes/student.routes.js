af899858d60b47b1628ac5970a64667c9d44350b
Ankita <deeparajput5348@gmail.com>
2024-12-25 00:53:51 +0530

    Added all jobs route

--- jobhub-project/Backend/src/routes/student.routes.js
+++ jobhub-project/Backend/src/routes/student.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { applyForJob } from "../controllers/student.controller.js";
+import {
+  applyForJob,
+  allJobsForStudents,
+} from "../controllers/student.controller.js";
 import { upload } from "../middleware/mutler.middleware.js";
 
 const router = Router();
 
 router.route("/applyjob").post(upload.single("resume"), applyForJob); //Pending cause of email
+router.route("/jobs").get(allJobsForStudents);
 
 export default router;
