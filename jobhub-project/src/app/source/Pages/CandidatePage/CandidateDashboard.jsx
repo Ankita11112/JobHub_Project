368ba22a779c91157fe5225d8502cc9df59a0d0a
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-01-07 15:43:57 +0530

    add

--- jobhub-project/src/app/source/Pages/CandidatePage/CandidateDashboard.jsx
+++ jobhub-project/src/app/source/Pages/CandidatePage/CandidateDashboard.jsx
@@ -1,50 +1,60 @@
 import { useState } from 'react';
 import CandidateDashboardNav from './Components/CandidateDashboardNavbar/CandidateDashboardNav';
 import { Outlet } from 'react-router-dom';
-import PageComponent from '../../../components/PageComponent/PageComponent'
+import PageComponent from '../../../components/PageComponent/PageComponent';
 import RegistrationPage from './RegistrationPage';
 import { Modal, Box, Typography } from '@mui/material';
 
-  
-
- 
 const CandidateDashboard = () => {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   const handleFormSubmit = () => {
     setIsFormSubmitted(true);
+    toast.success("Form submitted successfully!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
   return (
     <>
-     <Modal
+      {/* Modal for the Registration Page */}
+      <Modal
         open={!isFormSubmitted}
-        aria-labelledby='form-popup'
+        aria-labelledby="form-popup"
         closeAfterTransition
         disableEscapeKeyDown
       >
         <Box
-    
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            boxShadow: 24,
+            scrollbarWidth: 'none',
+            p: 4,
+            overflowY: 'auto', 
+            maxHeight: '80vh',
+            borderRadius: 2,
+          }}
         >
-          <Typography
-            id='form-popup'
-            variant='h6'
-            component='h2'
-            sx={{ mb: 2 }}
-          >
-            Please fill out the form
-          </Typography>
-          {/* Pass the submission handler to DemoForm */}
+          {/* Registration Form with Submit Handler */}
           <RegistrationPage onSubmit={handleFormSubmit} />
         </Box>
       </Modal>
+
+      {/* Main Dashboard Content */}
       <PageComponent>
         <CandidateDashboardNav />
         <Outlet />
       </PageComponent>
-      <RegistrationPage/>
     </>
   );
 };
 
 export default CandidateDashboard;
-
